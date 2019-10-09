027187b28e5357bc3bf56f1910bff72e4636371d
weeshqMZL <56204476+weeshqMZL@users.noreply.github.com>
2019-10-09 23:19:32 +0500

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,28 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  var mySrc = myImage.getAttribute('src');
+  if (mySrc === 'images/wk-icon.png') {
+    myImage.setAttribute ('src','images/skeleton-king.jpg');
+  } else {
+    myImage.setAttribute ('src','images/wk-icon.png');
+  }
+
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
